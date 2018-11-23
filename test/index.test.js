5f54a84a43958c304437068b32bf9b1a61edac1d
Alexandru Cambose <alexcambose@yahoo.com>
2018-11-23 23:20:31 +0200

    Enzyme configuration

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import InfiniteScroll from '../src/index';
+
 describe('main module', () => {
+  let component;
+  beforeEach(() => {
+    component = shallow(<InfiniteScroll />);
+  });
   it('props', () => {
     expect(true).toBeTruthy();
   });
