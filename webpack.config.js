350ff82a1bd06a5c305f80c35dd3f63edcb788f6
Alexandru Cambose <alexcambose@yahoo.com>
2018-11-30 00:07:33 +0200

    Build

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     vendor: ['react', 'react-dom', 'react-router', 'react-router-dom', 'redux', 'react-redux'],
   },
   output: {
-    path: path.resolve(__dirname, './dist/'),
+    path: path.resolve(__dirname, './'),
     filename: `[${inDev ? 'name' : 'hash'}].bundle.js`,
   },
   mode: inDev ? 'development' : 'production',
